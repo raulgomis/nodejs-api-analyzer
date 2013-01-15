a5385f31ef2c4a75db9e02176a0bf5e90cfaa6b3
javier.aznar <javier.aznar@eltallerdigital.com>
2013-01-15 12:37:36 +0100

    Jade

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-    var express = require('express');
-	var app = express();	
-	var sys = require("sys");
-    
-	
-	app.get('/', function(req, res){
-		res.send('Testing Express & Node js');
-	});
-	
-	app.get('/sayHello/:name', function(req, res){
-		res.send('Hello '+ req.params.name);
-	});
-	
-	
-	app.listen(8090);
+var express = require('express');
+var app = express();	
+var sys = require("sys");
+   
 
-    sys.puts("Server Running on 8090");   
+app.get('/', function(req, res){
+	res.send('Testing Express & Node js');
+});
+
+app.get('/sayHello/:name', function(req, res){
+	res.send('Hello '+ req.params.name);
+});
+
+
+app.listen(8090);
+
+sys.puts("Server Running on 8090");   
 	
 	
 	
