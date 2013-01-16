c5fdc6c2feb1731c462e85d631b85bdac149a9a9
javier.aznar <javier.aznar@eltallerdigital.com>
2013-01-16 08:58:12 +0100

    New web services

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var mongoose = require("mongoose");
+//mongoose.connect("mongodb://analyzer:analyzer@ds047447.mongolab.com:47447/analyzer");
+var db = mongoose.connection;
 
+// Config
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -23,12 +27,57 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+// Esquema
+var schema = mongoose.Schema({ 
+	date: { type: Date, default: Date.now },
+	username: String, 
+	exception: String,
+	stacktrace: String 
+});
+
+var CrashData = mongoose.model('CrashData', schema);
+
+// WS
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/sayHello/:name', function(req, res){
-	res.send('Hello '+ req.params.name);
+// Save crash
+app.get('/saveCrashLog', function(req, res){
+	var username = req.param('username', null);
+	var exception = req.param('exception', null);
+	var stacktrace = req.param('stacktrace', null);
+	
+	res.send('Crash detected! '+ username);
+	
+	
+	var crashLog = new CrashData({ 
+		username: username, 
+		exception: exception,
+		stacktrace: stacktrace
+		});
+		
+	crashLog.save(function (err) {
+	  	if (err) {
+			console.log('Error...');
+		}
+		else {
+	  		console.log('Crash saved for user: '+username);
+		}
+	});
+	
+});
+
+// List all crashes
+app.get('/listCrashLog', function(req, res){
+	var username = req.param('username', null);
+	
+	//TODO
+	//res.send("[{'username':'pepe'}]");
+	
+	res.send(
+		"['listado':[{'date':'dsadasdas', 'username':'asdasda','stacktrace':'asdasda','exception':'dasdasdas'},{'date':'dsadasdas', 'username':'asdasda', 'stacktrace':'asdasda', 'exception':'dasdasdas'}]]"
+	);
 });
 
 app.get('/', routes.index);
